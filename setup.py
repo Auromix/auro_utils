e499a4aa3331d1855a108a8999759ba02ae811c2
Herman Ye <hermanye233@icloud.com>
2025-03-25 17:22:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/Hermanye996/auro_utils",
-    version="0.0.7",
+    version="0.0.8",
     zip_safe=False,
 )
